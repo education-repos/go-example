df6a8e38b1613b420ed8ba331cb0bcc04c4e3489
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-06-04 12:18:12 +0400

    [feature] simple server

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "test")
+	w.Write([]byte("!!!"))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+
+	server := http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	fmt.Println("starting server at :8080")
+	server.ListenAndServe()
+}
